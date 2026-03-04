e7689e2219cb776735b649a4f080f6ae0a5d3113
brad <115906450+bradwils@users.noreply.github.com>
2026-03-05 07:56:46 +1100

    Update RemoteUser.swift
    Added enum for user stats within remote user

--- studyApp/Models/RemoteUser.swift
+++ studyApp/Models/RemoteUser.swift
@@ -6,13 +6,18 @@
 //
 
 
-//remote users only need to be identified by an ID, their displayname, and whether or not they're a friend. There isn't any sensitive data needed about them. if we want more info or something similar later down the line, we can just work with the ID. no need to pull everything.
+//Remote users only need an ID, display name, and friend status. Sensitive data isn’t required. If we need more information later, we can work with the ID. We don’t need to retrieve all their data.
 
 
 struct RemoteUser: Codable {
     let id: String // Unique ID from the server (todo)
     var displayName: String
+    var activeStatus: activeStatus //user state
     var isFriend: Bool
     var userPFPURL: String? //does not need to be grabbed everytime
     
+}
+
+enum activeStatus {
+    case offline, break, online, studying
 }
